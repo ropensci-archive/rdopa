86210c010dc043c87929a2a34bcb44cebd473261
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2014-06-11 22:00:35 +0300

    Test country ID input

--- tests/testthat/test-country.R
+++ tests/testthat/test-country.R
@@ -1,7 +1,25 @@
 context("get_country_species_count")
 
-test_that("returns the correct class", {
-  # China
+test_that("Arguments are handled correctly", {
+  # Argument country
+  expect_error(get_country_species_count(country=NA),
+               info="Using NA for country code should raise an error")
+  expect_error(get_country_species_count(country=NULL),
+               info="Using NULL for country code should raise an error")
+})
+
+test_that("Returns the correct class", {  
+  # China, country code should work as a string and numeric
   expect_is(get_country_species_count(country="156"), "numeric")
+  expect_is(get_country_species_count(country=156), "numeric")
   expect_is(get_country_species_count(country="Finland"), "numeric")
+})
+
+test_that("Non-existing country names and codes raise an error", {
+  expect_error(get_country_species_count(country="99999"),
+               info="Non-existant country code should raise an error")
+  expect_error(get_country_species_count(country=99999),
+               info="Non-existant country code should raise an error")
+  expect_error(get_country_species_count(country="FooBaristan"),
+               info="Non-existant country name should raise an error")
 })
