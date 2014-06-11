13e5def7a06b14604412081f2e5291664b177ebd
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2014-06-11 22:01:44 +0300

    Deal with country ID resolution

--- R/country.R
+++ R/country.R
@@ -30,5 +30,30 @@
 #'  
 #' }
 get_country_species_count <- function(country) {
-  NULL
+
+  # Check if country is string that can be coerced to a numeric
+  if (suppressWarnings(!is.na(as.numeric(country)))) {
+    country <- as.numeric(country)
+  }
+  
+  # If country is provided as a country name, try to convert it to a ISO code
+  if (is.character(country)) {
+    origin <- "country.name"
+    destination <- "iso3n"
+    # Must coerce to numeric, will give integer otherwise
+    code <- as.numeric(countrycode(country, "country.name", "iso3n"))
+    if (is.na(code)) {
+      stop("Country name ", country, " was not matched to an ISO code.")
+    }
+  } else if (is.numeric(country)) {
+    # Check that the ISO code exists
+    if (!country %in% countrycode_data$iso3n) {
+      stop("Country code ", country, " not a valid ISO 3166-1 code")
+    } else {
+      code  <- country
+    }
+  } else {
+    stop("country must be either string country name of numeric country code")
+  }
+  return(code)
 }
