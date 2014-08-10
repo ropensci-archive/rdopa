c56d8a1235750b0a3ad53bf6ccd4fc804fbbb4a2
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2014-08-10 18:49:05 +0300

    Add country_species_count checks

--- tests/testthat/test-01-country.R
+++ tests/testthat/test-01-country.R
@@ -8,8 +8,16 @@ test_that("Arguments are handled correctly", {
                info="Using NULL for country code should raise an error")
 })
 
-test_that("API values sane"), {
+test_that("API values sane", {
   # Slightly pointless check since we don't (and don't want to) check 
   # all the possible country/rlstatus combinations
-  expect_true(country_species_count(country="Finland", rlstatus="DD") >= 0)
+  expect_equal(country_species_count(country="Finland", rlstatus="DD"), 4)
+  expect_equal(country_species_count(country="Finland", rlstatus="EW"), 0)
+  expect_equal(country_species_count(country="Finland", rlstatus="EX"), 0)
+  expect_equal(country_species_count(country="Finland", rlstatus="DD"), 4)
+  expect_equal(country_species_count(country="Finland", rlstatus="LC"), 318)
+  expect_equal(country_species_count(country="Finland", rlstatus="NT"), 13)
+  expect_equal(country_species_count(country="Finland", rlstatus="VU"), 7)
+  expect_equal(country_species_count(country="Finland", rlstatus="EN"), 0)
+  expect_equal(country_species_count(country="Finland", rlstatus="CR"), 1)
 })
