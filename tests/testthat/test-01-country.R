e79db65cda1d44ea49882e35564577877d60d086
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2014-08-10 20:20:50 +0300

    Update tests, add cache

--- tests/testthat/test-01-country.R
+++ tests/testthat/test-01-country.R
@@ -11,13 +11,61 @@ test_that("Arguments are handled correctly", {
 test_that("API values sane", {
   # Slightly pointless check since we don't (and don't want to) check 
   # all the possible country/rlstatus combinations
-  expect_equal(country_species_count(country="Finland", rlstatus="DD"), 4)
-  expect_equal(country_species_count(country="Finland", rlstatus="EW"), 0)
-  expect_equal(country_species_count(country="Finland", rlstatus="EX"), 0)
-  expect_equal(country_species_count(country="Finland", rlstatus="DD"), 4)
-  expect_equal(country_species_count(country="Finland", rlstatus="LC"), 318)
-  expect_equal(country_species_count(country="Finland", rlstatus="NT"), 13)
-  expect_equal(country_species_count(country="Finland", rlstatus="VU"), 7)
-  expect_equal(country_species_count(country="Finland", rlstatus="EN"), 0)
-  expect_equal(country_species_count(country="Finland", rlstatus="CR"), 1)
+  expect_equal(country_species_count(country="Finland", rlstatus="DD",
+                                     cache=FALSE), 4)
+  expect_equal(country_species_count(country="Finland", rlstatus="EW",
+                                     cache=FALSE), 0)
+  expect_equal(country_species_count(country="Finland", rlstatus="EX",
+                                     cache=FALSE), 0)
+  expect_equal(country_species_count(country="Finland", rlstatus="DD",
+                                     cache=FALSE), 4)
+  expect_equal(country_species_count(country="Finland", rlstatus="LC",
+                                     cache=FALSE), 318)
+  expect_equal(country_species_count(country="Finland", rlstatus="NT",
+                                     cache=FALSE), 13)
+  expect_equal(country_species_count(country="Finland", rlstatus="VU",
+                                     cache=FALSE), 7)
+  expect_equal(country_species_count(country="Finland", rlstatus="EN",
+                                     cache=FALSE), 0)
+  expect_equal(country_species_count(country="Finland", rlstatus="CR",
+                                     cache=FALSE), 1)
+  # All species
+  expect_equal(country_species_count(country="Finland"), 343)
+})
+
+test_that("Cache works", {
+  
+  # Cache the response
+  cached_1 <- country_species_count(country="Finland", rlstatus="LC",
+                                   cache=TRUE)
+  # Ignore the cache
+  fresh_data <- country_species_count(country="Finland", rlstatus="LC",
+                                      cache=FALSE)
+  # Load from cache and compare to the previous data
+  cached_2 <- country_species_count(country="Finland", rlstatus="LC",
+                                    cache=TRUE)
+  expect_identical(cached_1, cached_2,
+                   "Cached values should be the same")
+  # Compare to the fresh data
+  expect_identical(cached_1, fresh_data,
+                   "Cached and fresh values should be the same")
+})
+
+context("country_species_list")
+
+test_that("Arguments are handled correctly", {
+  # Argument country
+  expect_error(country_species_list(country=NA),
+               info="Using NA for country code should raise an error")
+  expect_error(country_species_list(country=NULL),
+               info="Using NULL for country code should raise an error")
+})
+
+test_that("API values sane", {
+    
+  # Use New Zealand (country code = 554).
+  # First get all species and check the number
+  expect_equal(length(country_species_list(country="New Zealand", cache=FALSE)), 
+               463)
+  
 })
